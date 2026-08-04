a7098956c72362d74c7384728e668ad00f0a7677
David Rosenberg <targendaz2@me.com>
2026-08-04 13:04:02 -0300

    Add TypeBadge stories

---
+++ app/src/lib/components/data/type-badge.stories.svelte
@@ -0,0 +1,25 @@
+<script module>
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+  import TypeBadge from './type-badge.svelte';
+
+  const { Story } = defineMeta({
+    title: 'Data/TypeBadge',
+    component: TypeBadge,
+    argTypes: {
+      type: {
+        control: { type: 'select' },
+        options: ['rss', 'email', 'social', 'release', 'github'],
+      },
+    },
+  });
+</script>
+
+<Story name="RSS" args={{ type: 'rss' }} />
+
+<Story name="Email" args={{ type: 'email' }} />
+
+<Story name="Social" args={{ type: 'social' }} />
+
+<Story name="Release" args={{ type: 'release' }} />
+
+<Story name="GitHub" args={{ type: 'github' }} />
