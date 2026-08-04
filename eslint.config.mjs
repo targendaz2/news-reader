f21096acfdc4a539ba730327dfe93da58c5064af
David Rosenberg <targendaz2@me.com>
2026-08-03 22:15:10 -0300

    Updated eslint config to align with Svelte recommendations

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,45 @@
 // @ts-check
 
+import path from 'node:path';
+
+import js from '@eslint/js';
 import prettier from 'eslint-config-prettier';
 import storybook from 'eslint-plugin-storybook';
 import svelte from 'eslint-plugin-svelte';
+import { defineConfig, includeIgnoreFile } from 'eslint/config';
+import globals from 'globals';
+import ts from 'typescript-eslint';
 
-import js from '@eslint/js';
-import { defineConfig } from 'eslint/config';
-import tseslint from 'typescript-eslint';
+const gitignorePath = path.resolve(import.meta.dirname, '.gitignore');
 
 const config = defineConfig(
+  includeIgnoreFile(gitignorePath),
+  js.configs.recommended,
+  ts.configs.recommended,
+  svelte.configs.recommended,
+  prettier,
+  svelte.configs.prettier,
   {
-    files: ['**/*.{js,ts}'],
-    extends: [js.configs.recommended, tseslint.configs.recommended],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+    rules: {
+      'no-undef': 'off',
+    },
+  },
+  {
+    files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        extraFileExtensions: ['.svelte'],
+        parser: ts.parser,
+      },
+    },
   },
   {
     files: ['**/*.story.{js,ts,svelte}'],
     // @ts-ignore
     extends: [storybook.configs['flat/recommended']],
   },
-  prettier,
-  svelte.configs['flat/prettier'],
 );
 
 export default config;
