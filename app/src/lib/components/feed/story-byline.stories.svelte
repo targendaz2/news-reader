748a59aa59edcb7c6ff3c0deacc69455cb278b84
David Rosenberg <targendaz2@me.com>
2026-08-04 20:54:25 -0300

    Added stories for StoryByline component

---
+++ app/src/lib/components/feed/story-byline.stories.svelte
@@ -0,0 +1,21 @@
+<script module>
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+  import StoryByline from './story-byline.svelte';
+
+  const { Story } = defineMeta({
+    title: 'Feed/StoryByline',
+    component: StoryByline,
+    argTypes: {
+      type: {
+        control: { type: 'select' },
+        options: ['rss', 'email', 'social', 'release', 'github'],
+      },
+      source: { control: 'text' },
+      time: { control: 'text' },
+    },
+  });
+</script>
+
+<Story name="Release" args={{ type: 'release', source: 'Okta Release Notes', time: '2h' }} />
+
+<Story name="RSS" args={{ type: 'rss', source: 'Kotaku', time: '3h' }} />
