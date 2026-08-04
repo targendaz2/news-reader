da101779007c7dead0b7ca462c9b17c9cf2014d2
David Rosenberg <targendaz2@me.com>
2026-08-04 12:37:05 -0300

    Move svelte config out of vite config

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,19 +1,8 @@
-import adapter from '@sveltejs/adapter-cloudflare';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 const config = defineConfig({
-  plugins: [
-    sveltekit({
-      compilerOptions: {
-        // Force runes mode for the project, except for libraries. Can be removed in svelte 6.
-        runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true),
-        experimental: { async: true },
-      },
-      adapter: adapter(),
-      experimental: { remoteFunctions: true },
-    }),
-  ],
+  plugins: [sveltekit()],
 });
 
 export default config;
