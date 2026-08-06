e8f2c5ffad853591425466e415cff6db8b3cb1f6
David Rosenberg <targendaz2@me.com>
2026-08-06 17:24:06 -0300

    Add storybook stories for SearchBar component

---
+++ app/src/lib/components/controls/search-bar.stories.svelte
@@ -0,0 +1,19 @@
+<script module>
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+  import SearchBar from './search-bar.svelte';
+
+  const { Story } = defineMeta({
+    title: 'Design System/Controls/SearchBar',
+    component: SearchBar,
+    argTypes: {
+      value: { control: 'text' },
+      placeholder: { control: 'text' },
+    },
+  });
+</script>
+
+<!-- Empty, showing the placeholder -->
+<Story name="Empty" args={{ value: '' }} />
+
+<!-- With a query already typed in -->
+<Story name="With Query" args={{ value: 'macOS' }} />
