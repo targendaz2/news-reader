ee32e9d97cab64b9a4e65934834a93cee3943e67
David Rosenberg <targendaz2@me.com>
2026-08-07 19:29:30 -0300

    Add storybook stories for FilterPills component

---
+++ app/src/lib/components/controls/filter-pills.stories.svelte
@@ -0,0 +1,15 @@
+<script module>
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+  import FilterPills from './filter-pills.svelte';
+
+  const { Story } = defineMeta({
+    title: 'Design System/Controls/FilterPills',
+    component: FilterPills,
+  });
+</script>
+
+<Story name="All" args={{ active: 'all' }} />
+
+<Story name="Unread" args={{ active: 'unread' }} />
+
+<Story name="Saved" args={{ active: 'saved' }} />
