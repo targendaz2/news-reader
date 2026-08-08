9250d5088658ba4f514e0d600503b05dbe9ae437
David Rosenberg <targendaz2@me.com>
2026-08-08 02:31:56 -0300

    Add storybook stories for LoadingIndicator component

---
+++ app/src/lib/components/feedback/loading-indicator.stories.svelte
@@ -0,0 +1,16 @@
+<script module>
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+  import LoadingIndicator from './loading-indicator.svelte';
+
+  const { Story } = defineMeta({
+    title: 'Design System/Feedback/LoadingIndicator',
+    component: LoadingIndicator,
+    argTypes: {
+      label: { control: 'text' },
+    },
+  });
+</script>
+
+<Story name="Default" />
+
+<Story name="Custom Label" args={{ label: 'Loading story…' }} />
